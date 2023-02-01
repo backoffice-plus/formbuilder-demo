ab6158f9dd0d3fa5875c4a14ef9a95c646e2754f
David Wolter <david@dampfer.net>
2023-02-01 12:33:32 +0100

    fix tailwind

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import '@backoffice-plus/formbuilder/src/css/tailwind.css'
 import '@backoffice-plus/formbuilder/src/css/form.stylea.css'
+import './assets/style.css'
 
 //@ts-ignore
 import App from './App.vue'
