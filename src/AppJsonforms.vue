e6682d5f6ff3ec284f5c7d5ae5237679a4822b95
Julian Haupt <julian@haupt.media>
2023-02-13 16:48:30 +0100

    updated to most recent formbuilder version

---
+++ src/AppJsonforms.vue
@@ -0,0 +1,90 @@
+<template>
+
+  <div class="container max-w-screen-lg mx-auto p-4 flex flex-col gap-4">
+
+    <div class="flex items-baseline gap-2"><h2>{{ example }}</h2> <a :href="'#/'+ url.search" class=" text-sm">[back]</a></div>
+
+
+    <details open="true">
+      <summary class="cursor-pointer">JsonForms</summary>
+      <div class="card p-4">
+        <JsonForms
+            :class="'styleA'"
+            :schema="jsonForms.schema"
+            :uischema="jsonForms.uischema"
+            :data="jsonFormsData"
+            :renderers="jsonFormRenderesMore"
+            :ajv="ajv"
+            @change="r => jsonFormsUpdated=r"
+        />
+      </div>
+    </details>
+
+    Data
+    <textarea class="w-full h-60 p-4 bg-white rounded" readonly disabled>{{ jsonFormsUpdated?.data }}</textarea>
+
+    <details open="true">
+      <summary class="cursor-pointer">JSON</summary>
+      <div class="card p-4">
+        <SchemaCode
+            v-model:schema="jsonForms.schema"
+            v-model:uischema="jsonForms.uischema"
+        />
+      </div>
+    </details>
+
+  </div>
+
+</template>
+
+<style>
+
+</style>
+
+
+<script setup>
+import {computed, ref} from 'vue'
+import {JsonForms} from "@jsonforms/vue";
+import {createAjv, generateDefaultUISchema} from "@jsonforms/core";
+import SchemaCode from './SchemaCode.vue'
+import {useJsonforms} from "@backoffice-plus/formbuilder";
+import {getExamples} from "@jsonforms/examples/src";
+import {getExampleFromUrl, getUrl} from "./lib";
+import {vanillaRenderers} from "@jsonforms/vue-vanilla";
+import {boplusVueVanillaRenderers} from "@backoffice-plus/formbuilder";
+
+const url = computed(() => getUrl());
+const examples = computed(() => getExamples().sort((a, b) => a.label.toLowerCase() > b.label.toLowerCase() ? 1 : -1));
+const example = ref(getExampleFromUrl());
+
+
+const {schema, uischema} = useJsonforms();
+const jsonFormsSchema = schema;
+const jsonFormsUiSchema = uischema;
+const jsonFormsData = ref({});
+const jsonFormsUpdated = ref({});
+
+const jsonFormRenderes = Object.freeze([
+  ...vanillaRenderers,
+  ...boplusVueVanillaRenderers,
+]);
+
+
+const jsonForms = computed(() => {
+  const exampleData = getExamples().find(item => item.name === example.value);
+
+  if (!exampleData?.uischema) {
+    exampleData.uischema = generateDefaultUISchema(exampleData.schema)
+  }
+
+  return exampleData
+});
+
+const jsonFormRenderesMore = Object.freeze([
+  ...jsonFormRenderes,
+]);
+
+const ajv = createAjv();//is needed because reactive :schema & :uischema will throw error
+
+</script>
+
